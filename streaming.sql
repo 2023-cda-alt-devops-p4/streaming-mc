f25949ce482ae64cfc2e6e2451e7f7399c6ccc89
Melcn <melaniearinosy@outlook.com>
2023-10-02 16:52:52 +0200

    feat streamingsql: update filmmaker

--- streaming.sql
+++ streaming.sql
@@ -57,32 +57,26 @@ VALUES ('Almodóvar', 'Pedro'),
 
 -- Almodóvar
 INSERT INTO Film (title_film, actors_film, film_maker, duration, release_year)
-VALUES ('Parle avec elle', 'Javier Cámara, Darío Grandinetti, Leonor Watling', 'Almodóvar', 112, '2002');
-
--- Tarantino
-INSERT INTO Film (title_film, actors_film, film_maker, duration, release_year)
-VALUES ('Pulp Fiction', 'John Travolta, Samuel L. Jackson, Uma Thurman', 'Tarantino', 154, '1994');
-
--- Eastwood
-INSERT INTO Film (title_film, actors_film, film_maker, duration, release_year)
-VALUES ('Impitoyable', 'Clint Eastwood, Gene Hackman, Morgan Freeman', 'Eastwood', 131, '1992');
+VALUES  ('Parle avec elle', 'Javier Cámara, Darío Grandinetti, Leonor Watling', 'Almodóvar', 112, '2002'),
+        ('Pulp Fiction', 'John Travolta, Samuel L. Jackson, Uma Thurman', 'Tarantino', 154, '1994'),
+        ('Impitoyable', 'Clint Eastwood, Gene Hackman, Morgan Freeman', 'Eastwood', 131, '1992');
 
 
 -- utilisateur
 INSERT INTO Users (name_user, first_name_user, email_user, password_user, role_user, favorite_film)
-VALUES ('Smith', 'John', 'john.smith@email.com', 'motdepasse1', 'Utilisateur', 'Parle avec elle');
+VALUES  ('Smith', 'John', 'john.smith@email.com', 'motdepasse1', 'Utilisateur', 'Parle avec elle');
 
 
 INSERT INTO Actors (name_actor, first_name_actor, role, birth_date)
-VALUES ('Cámara', 'Javier', 'Benigno', '1970-01-01'),
-       ('Grandinetti', 'Darío', 'Marco', '1969-02-15'),
-       ('Watling', 'Leonor', 'Alicia', '1976-12-28'),
-       ('Travolta', 'John', 'Vincent Vega', '1954-02-18'),
-       ('Jackson', 'Samuel L.', 'Jules Winnfield', '1948-12-21'),
-       ('Thurman', 'Uma', 'Mia Wallace', '1970-04-29'),
-       ('Eastwood', 'Clint', 'Bill Munny', '1930-05-31'),
-       ('Hackman', 'Gene', 'Little Bill Daggett', '1930-01-30'),
-       ('Freeman', 'Morgan', 'Ned Logan', '1937-06-01');
+VALUES  ('Cámara', 'Javier', 'Benigno', '1970-01-01'),
+        ('Grandinetti', 'Darío', 'Marco', '1969-02-15'),
+        ('Watling', 'Leonor', 'Alicia', '1976-12-28'),
+        ('Travolta', 'John', 'Vincent Vega', '1954-02-18'),
+        ('Jackson', 'Samuel L.', 'Jules Winnfield', '1948-12-21'),
+        ('Thurman', 'Uma', 'Mia Wallace', '1970-04-29'),
+        ('Eastwood', 'Clint', 'Bill Munny', '1930-05-31'),
+        ('Hackman', 'Gene', 'Little Bill Daggett', '1930-01-30'),
+        ('Freeman', 'Morgan', 'Ned Logan', '1937-06-01');
 
 
 
