838d9a63b4425c9ba33fd88326f370fdbaba9917
Melcn <melaniearinosy@outlook.com>
2023-10-02 16:50:28 +0200

    feat streamingsql: insert actor in actors

--- streaming.sql
+++ streaming.sql
@@ -73,4 +73,16 @@ INSERT INTO Users (name_user, first_name_user, email_user, password_user, role_u
 VALUES ('Smith', 'John', 'john.smith@email.com', 'motdepasse1', 'Utilisateur', 'Parle avec elle');
 
 
+INSERT INTO Actors (name_actor, first_name_actor, role, birth_date)
+VALUES ('Cámara', 'Javier', 'Benigno', '1970-01-01'),
+       ('Grandinetti', 'Darío', 'Marco', '1969-02-15'),
+       ('Watling', 'Leonor', 'Alicia', '1976-12-28'),
+       ('Travolta', 'John', 'Vincent Vega', '1954-02-18'),
+       ('Jackson', 'Samuel L.', 'Jules Winnfield', '1948-12-21'),
+       ('Thurman', 'Uma', 'Mia Wallace', '1970-04-29'),
+       ('Eastwood', 'Clint', 'Bill Munny', '1930-05-31'),
+       ('Hackman', 'Gene', 'Little Bill Daggett', '1930-01-30'),
+       ('Freeman', 'Morgan', 'Ned Logan', '1937-06-01');
+
+
 
