cdeeb7ba7d58551e4dd22c5377373aa6bd30d8e1
Melcn <melaniearinosy@outlook.com>
2023-10-04 14:35:50 +0200

    feat(data): fix property

--- data.sql
+++ data.sql
@@ -1,12 +1,11 @@
--- Almodóvar
-INSERT INTO Film (title_film, actors_film, film_maker, duration, release_year)
+
+INSERT INTO Film (title_film, actors_film, film_maker, duration, release_date)
 VALUES  ('Parle avec elle', 'Javier Cámara, Darío Grandinetti, Leonor Watling', 'Almodóvar', 112, '2002-01-01'),
         ('Pulp Fiction', 'John Travolta, Samuel L. Jackson, Uma Thurman', 'Tarantino', 154, '1994-01-01'),
         ('Impitoyable', 'Clint Eastwood, Gene Hackman, Morgan Freeman', 'Eastwood', 131, '1992-01-01');
 
 
--- utilisateur
-INSERT INTO Users (name_user, first_name_user, email_user, password_user, role_user, favorite_film)
+INSERT INTO Users (Name_user, FirstName_user, Email_user, Password_user, role_user, favorite_film)
 VALUES  ('Smith', 'John', 'john.smith@email.com', 'motdepasse1', 'Utilisateur', 'Parle avec elle'),
         ('Johnson', 'Sarah', 'sarah.johnson@email.com', 'motdepasse2', 'Utilisateur', 'Pulp Fiction'),
         ('Davis', 'Michael', 'michael.davis@email.com', 'motdepasse3', 'Utilisateur', 'Impitoyable'),
@@ -19,7 +18,7 @@ VALUES  ('Smith', 'John', 'john.smith@email.com', 'motdepasse1', 'Utilisateur', 
         ('Thomas', 'Jennifer', 'jennifer.thomas@email.com', 'motdepasse10', 'Utilisateur', 'Parle avec elle');
 
 
-INSERT INTO Actors (name_actor, first_name_actor, role, birth_date)
+INSERT INTO Actor (name_actor, firstName_actor, role_actor, birthDate)
 VALUES  ('Cámara', 'Javier', 'Benigno', '1970-01-01'),
         ('Grandinetti', 'Darío', 'Marco', '1969-02-15'),
         ('Watling', 'Leonor', 'Alicia', '1976-12-28'),
@@ -29,6 +28,12 @@ VALUES  ('Cámara', 'Javier', 'Benigno', '1970-01-01'),
         ('Eastwood', 'Clint', 'Bill Munny', '1930-05-31'),
         ('Hackman', 'Gene', 'Little Bill Daggett', '1930-01-30'),
         ('Freeman', 'Morgan', 'Ned Logan', '1937-06-01');
+
+INSERT INTO diretor (name_diretor, firstName_diretor)
+VALUES ('Almodóvar', 'Pedro'),
+       ('Tarantino', 'Quentin'),
+       ('Eastwood', 'Clint');
+
 
 
 
