72def32b556c21bdac19511ab5d7d33a7f574c57
Melcn <melaniearinosy@outlook.com>
2023-10-05 14:28:44 +0200

    feat(sql):fix insert id auto increment

--- streaming.sql
+++ streaming.sql
@@ -1,50 +1,45 @@
 CREATE TABLE Film(
-   id_film SERIAL INT,
+   id_film SERIAL PRIMARY KEY,
    title_film VARCHAR(60),
    actors_film VARCHAR(50),
    film_maker VARCHAR(50),
    duration INT,
-   release_date DATE,
-   PRIMARY KEY(id_film)
+   release_date DATE
 );
 
 CREATE TABLE Actor(
-   id_actor SERIAL INT,
+   id_actor SERIAL PRIMARY KEY,
    name_actor VARCHAR(50),
    firstName_actor VARCHAR(50),
    role_actor VARCHAR(50),
-   birthDate DATE,
-   PRIMARY KEY(id_actor)
+   birthDate DATE
 );
 
 CREATE TABLE diretor(
-   id_diretor SERIAL INT,
+   id_diretor SERIAL PRIMARY KEY,
    name_diretor VARCHAR(50),
    firstName_diretor VARCHAR(50),
-   id_film INT NOT NULL,
-   PRIMARY KEY(id_diretor),
+   id_film INT NOT NULL
    FOREIGN KEY(id_film) REFERENCES Film(id_film)
 );
 
 CREATE TABLE Users(
-   Id_user SERIAL INT,
+   Id_user SERIAL PRIMARY KEY,
    Name_user VARCHAR(50),
    FirstName_user VARCHAR(50),
    Email_user VARCHAR(50),
    Password_user VARCHAR(50),
    Role_user VARCHAR(50),
-   Favorite_film VARCHAR(50),
-   PRIMARY KEY(Id_user)
+   Favorite_film VARCHAR(50)
 );
 
 CREATE TABLE Archive(
-   id_archive SERIAL INT,
+   id_archive SERIAL PRIMARY KEY,
    Date_archive DATE,
    Update_archive DATE,
    odl_value VARCHAR(50),
    new_value VARCHAR(50),
    Id_user INT NOT NULL,
-   PRIMARY KEY(id_archive),
    FOREIGN KEY(Id_user) REFERENCES Users(Id_user)
 );
 
